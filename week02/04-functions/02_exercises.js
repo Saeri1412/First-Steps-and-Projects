9a919811cf6247876dd73eab07edd2f5d284c6e2
Sarah <sarahvictoria.elser@gmail.com>
2021-01-11 22:05:31 +0100

    added solution for exercise 3

--- week02/04-functions/02_exercises.js
+++ week02/04-functions/02_exercises.js
@@ -48,3 +48,38 @@ setTimeout(function () {
          console.log('Goodbye!');
      }); // logs 'Goodbye!' 1.5 seconds later
  }); // logs 'Hello!' 1.5 seconds later */
+
+/* 3
+
+Write a function that expects a number as an argument. 
+If the value that is passed in is less than 0, equal to 0, or not a number, 
+the function should return the string 'ERROR'. 
+If the number that is passed in is greater than or equal to 1000000 it should simply return the number. 
+Otherwise it should multiply the number by 10 however many times it takes to get a number that 
+is greater than or equal to 1000000 and return that. */
+
+function myFn(num) {
+    if (num <= 0 || Number.isNaN(num) || typeof num === "number") {
+        console.log("ERROR");
+    } else if (num >= 1000000) {
+        console.log(num);
+    } else {
+        while (num >= 1000000) {
+            num = num * 10;
+        }
+        return num;
+    }
+}
+myFn(2);
+
+/*function countdown(num) {
+        console.log(num);
+
+    
+        // break condition
+        if (num > 1) {
+            countdown(num - 1); // function calls itself
+        }
+    }
+    
+    countdown(10);*/
