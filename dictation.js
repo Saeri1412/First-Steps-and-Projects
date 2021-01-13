fb92aab22a1a7b369ced6a9fb5f94c4b6eb9346c
Sarah <sarahvictoria.elser@gmail.com>
2021-01-13 13:46:59 +0100

    dictation is fun

---
+++ dictation.js
@@ -0,0 +1,22 @@
+var x = 1;
+
+var doubleX;
+
+function timesTwo(num) {
+    return num * 2;
+} 
+
+console.log(timesTwo(x));
+
+var numbers = {
+    y: doubleX
+};
+
+var numbers = ([x, doubleX]); 
+ for(i = 0; i <= numbers.length, i++);
+
+ console.log(numbers[]);
+
+
+
+ 
