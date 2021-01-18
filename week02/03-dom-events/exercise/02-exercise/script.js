0912fa535e21c77c9896c4b9f543d09bb6832c4a
Sarah <sarahvictoria.elser@gmail.com>
2021-01-18 11:56:52 +0100

    added solution for exercise 2

--- week02/03-dom-events/exercise/02-exercise/script.js
+++ week02/03-dom-events/exercise/02-exercise/script.js
@@ -7,29 +7,9 @@ the characters should be replaced with the characters in the corresponding posit
 var address =
     "Address Delivered at the Dedication of the Cemetery at Gettysburg Abraham Lincoln November 19, 1863.\nFour score and seven years ago our fathers brought forth on this continent, a new nation, conceived in Liberty, and dedicated to the proposition that all men are created equal.Now we are engaged in a great civil war, testing whether that nation, or any nation so conceived and dedicated, can long endure. We are met on a great battle-field of that war. We have come to dedicate a portion of that field, as a final resting place for those who here gave their lives that that nation might live. It is altogether fitting and proper that we should do this.But, in a larger sense, we can not dedicate -- we can not consecrate -- we can not hallow -- this ground. The brave men, living and dead, who struggled here, have consecrated it, far above our poor power to add or detract. The world will little note, nor long remember what we say here, but it can never forget what they did here. It is for us the living, rather, to be dedicated here to the unfinished work which they who fought here have thus far so nobly advanced. It is rather for us to be here dedicated to the great task remaining before us -- that from these honored dead we take increased devotion to that cause for which they gave the last full measure of devotion -- that we here highly resolve that these dead shall not have died in vain -- that this nation, under God, shall have a new birth of freedom -- and that government of the people, by the people, for the people, shall not perish from the earth.";
 
-var textarea = function getElemArrayByClassName(className) {
-    var element = document.getElementsByClassName(className);
-    var address = [];
-    for (var i = 0; i < address.length; i++) {
-        a.push(address[i]);
-    }
-    return a;
-};
-
-console.log(getElemArrayByClassName);
-
-/*var textarea = document.getElementById("gettyburry").value;
-
-textarea.addEventListener("input", function (event) {
-    console.log(event.target.value);
-
-    if (event.target.value === "") {
-        event.target.value = "";
-    }
-});d
-
-
-ocument.getElementsByTagName('TEXTAREA')[0].addEventListener('input', function(e) {
-    var targ = e.target;
-    targ.value = address.slice(0, targ.value.length);
-});*/
+document
+    .getElementsByTagName("TEXTAREA")[0]
+    .addEventListener("input", function (e) {
+        var targ = e.target;
+        targ.value = address.slice(0, targ.value.length);
+    });
